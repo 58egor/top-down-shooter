8c0b690266894d36cdc9d2e8d8393c886bcd3e16
ERRORI404 <68480726+ERRORI404@users.noreply.github.com>
2020-09-14 20:10:56 +0300

    combined the scripts into one
    combined the reload and chage scripts  into one

---
+++ top down shooter/Assets/NULLcode Studio/ASCamera/Scripts/CameraControl.cs
@@ -0,0 +1,79 @@
+﻿// NULLcode Studio © 2016
+// null-code.ru
+
+using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	[Header("Трансформ персонажа:")]
+	public Transform player;
+
+	[Header("Настройки высоты:")]
+	public float maxHeight = 20;
+	public float minHeight = 10;
+
+	[Header("Настройки вращения:")]
+	public KeyCode rotateA = KeyCode.Q;
+	public KeyCode rotateB = KeyCode.E;
+	public float maxRotate = 60;
+	public float minRotate = -60;
+
+	[Header("Общий параметр сглаживания:")]
+	public float smooth = 2;
+
+	private float height, rotIndex;
+
+	void Awake()
+	{
+		height = maxHeight;
+		transform.position = player.position + new Vector3(0, height, 0);
+	}
+
+	void LateUpdate()
+	{
+		DoTransform(); // следование за игроком
+		DoRotation(); // поворот камеры
+	}
+
+	void DoRotation()
+	{
+		if(Input.GetKey(rotateA)) rotIndex += 1;
+		else if(Input.GetKey(rotateB)) rotIndex -= 1;
+
+		rotIndex = Mathf.Clamp(rotIndex, minRotate, maxRotate);
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, rotIndex, 0), smooth * Time.deltaTime);
+	}
+
+	void DoTransform()
+	{
+		if(!player) return;
+
+		if(Input.GetAxis("Mouse ScrollWheel") > 0) height -= 1;
+		else if(Input.GetAxis("Mouse ScrollWheel") < 0) height += 1;
+
+		height = Mathf.Clamp(height, minHeight, maxHeight);
+
+		// берем текущее положение курсора
+		Vector3 mouse = Input.mousePosition;
+
+		// находим позицию персонажа в пространстве экрана
+		Vector3 playerPos = Camera.main.WorldToScreenPoint(player.position);
+
+		// создаем окно, уменьшившую в два раза копию текущего, и закрепляем его за позицией персонажа
+		Rect rect = new Rect(playerPos.x - (Screen.width/2)/2, playerPos.y - (Screen.height/2)/2, Screen.width/2, Screen.height/2);
+
+		// удерживание позиции курсора в рамках окна
+		mouse = Vector2.Max(mouse, rect.min);
+		mouse = Vector2.Min(mouse, rect.max);
+
+		// переносим позицию курсора в мировые координаты
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(mouse.x, mouse.y, transform.position.y));
+
+		// находим центр, между персонажем и измененной позиций курсора
+		Vector3 camLook = (player.position + mousePos)/2;
+
+		// финальная позиция камеры
+		transform.position = Vector3.Lerp(transform.position, new Vector3(camLook.x, player.position.y + height, camLook.z), smooth * Time.deltaTime);
+	}
+}
---
+++ top down shooter/Assets/scripts/ChangeReload.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeReload : MonoBehaviour
+{
+    [Header("Имя оружия:")]
+    public string Name = "Revolver2";
+    [Header("Информация о патронах:")]
+    public int Ammo = 48;
+    public int AddAmmo = 24;
+    bool ready = false;
+    [Header("Игрок:")]
+    public GameObject player;
+    [Header("ай ди пушки из скрипта change gun:")]
+    public int GunId = 3;
+    [Header("длительность перезарядки:")]
+    public float LongOfChageDefault = 0f;
+    float LongOfChage;
+    bool Have = false;
+    [Header("тип патронов:")]
+    public  bool PistolAmmo=false;
+    public bool SMGAmmo = false;
+    public bool ShootgunAmmo = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LongOfChage = LongOfChageDefault;
+    }
+    private void OnTriggerEnter(Collider other)
+    {//проверяем зашел ли игрок врадиус тригерра,если да то разрешить добавление патронов и смену оружия
+        if (other.tag == "Player")
+        {
+            player = other.gameObject;
+            ready = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {//если игрок вышел то запрет
+        if (other.tag == "Player")
+        {
+            ready = false;
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (ready)
+        {
+            Change();
+            Reload();
+        }
+    }
+    //функция отвечающа за смену оружия
+    void Change()
+    {
+        //проверяем есть ли пушка с таким же именем
+        int length = player.GetComponent<ChangeGun>().gun.Length;
+        for (int i = 0; i < length; i++)
+        {
+            if (player.GetComponent<ChangeGun>().gun[i].GetComponent<GunInfo>().Name == Name)
+            {
+                Have = true;
+                break;
+            }
+            else Have = false;
+        }
+        Debug.Log(Have);
+        //если нет и была нажата кнопка е то меняем оружие
+        if (Have == false && Input.GetKeyDown(KeyCode.E))
+        {
+            int i = player.GetComponent<ChangeGun>().Id;//узнаем место в массиве которое должны поменять
+            ChangeGun info = player.GetComponent<ChangeGun>();
+            info.gun[i].SetActive(false);//деактивируем оружие
+           // info.gun[i].GetComponent<PistolRay>().removed();
+            Instantiate(info.DropGun[info.gun[i].GetComponent<GunInfo>().Id], info.DotSpawn.transform.position, info.DropGun[info.gun[i].GetComponent<GunInfo>().Id].transform.rotation);//спавним пушку которую держали
+            info.gun[i] = info.Allguns[GunId];//записываем в массив новое оружие
+            player.GetComponent<ChangeGun>().Change();
+            Destroy(gameObject);
+            Debug.Log("done");
+        }
+        else
+        {
+            LongOfChage = LongOfChageDefault;
+        }
+    }
+    //функция добавления патронов
+    void Reload()
+    {
+        ChangeGun info = player.GetComponent<ChangeGun>();
+        //если пистолет
+        if (PistolAmmo)
+        {
+            //если патронов не максимум то добавляем
+            if (info.PistolAmmo != info.PistolAmmoMax)
+            {//если в результате будет максимум то просто привавнимаем к нему
+                if (info.PistolAmmo + AddAmmo > info.PistolAmmoMax)
+                {
+                    info.PistolAmmo = info.PistolAmmoMax;
+                }
+                else
+                {//иначе добалвяем
+                    info.PistolAmmo += AddAmmo;
+                }
+                AddAmmo = 0;
+                //Destroy(gameObject);
+            }
+        }
+        //если пп/автомат
+        if (SMGAmmo)
+        {
+            if (info.SMGAmmo != info.SMGAmmoMax)
+            {
+                if (info.SMGAmmo + AddAmmo > info.SMGAmmoMax)
+                {
+                    info.SMGAmmo = info.SMGAmmoMax;
+                }
+                else
+                {
+                    info.SMGAmmo += AddAmmo;
+                }
+                AddAmmo = 0;
+                //Destroy(gameObject);
+            }
+        }
+        //если дробовик
+        if (ShootgunAmmo)
+        {
+            if (info.ShootgunAmmo != info.ShootgunAmmoMax)
+            {
+                if (info.ShootgunAmmo + AddAmmo > info.ShootgunAmmoMax)
+                {
+                    info.ShootgunAmmo = info.ShootgunAmmoMax;
+                }
+                else
+                {
+                    info.ShootgunAmmo += AddAmmo;
+                }
+                AddAmmo = 0;
+                //Destroy(gameObject);
+            }
+        }
+    }
+}
